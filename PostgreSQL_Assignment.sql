7495f5b05cd2d5f130ea60605b218be1559ceb7a
SanimSRk <hasansanim562@gmail.com>
2025-03-24 23:11:48 +0600

    every thing is done

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -32,8 +32,8 @@ CREATE TABLE customers(
  --------- add  customers valuse table  for db -----------
 
 INSERT INTO customers (name, email) VALUES
-('John Doe', 'john@example.com'),
-('Jane Smith', 'jane@example.com');
+('Hasan Mia', 'hasan@example.com'),
+('Sanim Hasan', 'sanim@example.com');
 
 --- ------this commman run show the all customers tabile data----------
 
@@ -54,9 +54,47 @@ CREATE TABLE orders (
  --------- add  orders valuse table  for db -----------
 
 INSERT INTO orders (customer_id, book_id, quantity) VALUES
-(1, 2, 3),
-(2, 1, 1);
+(5, 3, 2)
+
 
 --- ------this commman run show the all orders tabile data----------
 
 SELECT * FROM orders
+
+------- PostgreSQL Problems & Sample Outputs---------------------------
+
+
+--01/--Find books that are out of stock.-------
+SELECT title FROM books WHERE stock=0
+
+--02/--Retrieve the most expensive book in the store.-------
+SELECT * FROM books ORDER BY price DESC LIMIT 1
+
+---3/-- Find the total number of orders placed by each customer.----------
+SELECT name, COUNT(*) AS total_orders
+FROM customers
+JOIN orders ON customers.id = customer_id
+GROUP BY name
+
+
+---4/-- total_revenue.----------
+SELECT sum(price*orders.quantity) AS total_revenuo
+FROM books
+JOIN orders ON books.id = book_id
+
+---5/-- Calculate the total revenue generated from book sales..----------
+SELECT name, COUNT(*) AS  orders_count
+FROM customers
+JOIN orders ON customers.id = customer_id
+GROUP BY name
+HAVING count(*)>1
+
+---6/-- Find the average price of books in the store.----------
+SELECT AVG(price) AS avg_book_price  FROM books
+
+---7/-- Increase the price of all books published before 2000 by 10%.---------
+UPDATE books SET price=price*1.10 WHERE published_year<2000
+
+---8/-- Delete customers who haven't placed any orders.---------
+DELETE FROM customers
+WHERE id NOT IN (SELECT DISTINCT customer_id FROM orders);
